ea7b5d7c705b7006381923a320194f224dd7b459
marneymallory <79782193+marneymallory@users.noreply.github.com>
2021-04-15 12:47:50 -0800

    fail 2nd test (good fail
    Co-authored-by: Cassandra Copp <cass16180@gmail.com>

--- _tests_/haiku.test.js
+++ _tests_/haiku.test.js
@@ -7,4 +7,17 @@ import Haiku from './../src/haiku.js';
   expect(haiku.line2).toMatch("line2");
   expect(haiku.line3).toMatch("line3");
   });
+
+  test('should correctly determine if line is a string', () => {
+  const haiku = new Haiku("line1","line2","line3");
+  haiku.line1.isString();
+  expect(true);
+  });
+
+  // test('should correctly determine if line1 is not a string', () => {
+  //   haiku.isString();
+  //   expect(false);
+  //   });
+
+
 });
